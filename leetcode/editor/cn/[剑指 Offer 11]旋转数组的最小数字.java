fdf53d49f6e9548b1c153b75476616a274be7877
daixiaojun <daixiaojun@innotechx.com>
2021-06-11 19:08:15 +0800

    no message

---
+++ leetcode/editor/cn/[剑指 Offer 11]旋转数组的最小数字.java
@@ -0,0 +1,35 @@
+//把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2
+//] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。 
+//
+// 示例 1： 
+//
+// 输入：[3,4,5,1,2]
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 输入：[2,2,2,0,1]
+//输出：0
+// 
+//
+// 注意：本题与主站 154 题相同：https://leetcode-cn.com/problems/find-minimum-in-rotated-sor
+//ted-array-ii/ 
+// Related Topics 二分查找 
+// 👍 329 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minArray(int[] numbers) {
+        //如果numbers[i + 1] < numbers[i], i + 1便是排序数组的头
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] > numbers[i + 1]) {
+                return numbers[i + 1];
+            }
+        }
+
+        return numbers[0];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
