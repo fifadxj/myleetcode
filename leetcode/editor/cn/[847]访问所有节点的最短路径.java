f88bbc28fd0d5688e4788c0decb02729aee3be9d
Terry <dxjwy1@sina.com>
2021-08-06 22:08:54 +0800

    no message

--- leetcode/editor/cn/[847]访问所有节点的最短路径.java
+++ leetcode/editor/cn/[847]访问所有节点的最短路径.java
@@ -46,28 +46,36 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution {//看了题解也不会做。。照抄官方题解
     public int shortestPathLength(int[][] graph) {
-        int[][] visited = new int[graph.length][graph[0].length];
-        Queue<Node> queue = new LinkedList();
-        int step = 0;
+        int n = graph.length;
+        Queue<int[]> queue = new LinkedList<int[]>();
+        boolean[][] seen = new boolean[n][1 << n];
+        for (int i = 0; i < n; ++i) {
+            queue.offer(new int[]{i, 1 << i, 0});
+            seen[i][1 << i] = true;
+        }
 
-        while (! queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Node node = queue.poll();
-                int[] nexts = graph[node.num];
-                for (int j = 0; j < nexts.length; j++) {
-                    int status = node.status |
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int[] tuple = queue.poll();
+            int u = tuple[0], mask = tuple[1], dist = tuple[2];
+            if (mask == (1 << n) - 1) {
+                ans = dist;
+                break;
+            }
+            // 搜索相邻的节点
+            for (int v : graph[u]) {
+                // 将 mask 的第 v 位置为 1
+                int maskV = mask | (1 << v);
+                if (!seen[v][maskV]) {
+                    queue.offer(new int[]{v, maskV, dist + 1});
+                    seen[v][maskV] = true;
                 }
             }
-
         }
-    }
-
-    static class Node {
-        int num;
-        int status;
+        return ans;
     }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
